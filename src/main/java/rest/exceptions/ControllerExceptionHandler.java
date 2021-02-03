04e7135c26d77a7f7a4ca658e9c819f7c3687e52
iealeshin <iealeshin@domclick.ru>
2021-02-03 10:57:57 +0300

    ref: response body

--- src/main/java/rest/exceptions/ControllerExceptionHandler.java
+++ src/main/java/rest/exceptions/ControllerExceptionHandler.java
@@ -8,9 +8,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
+import rest.models.dto.DealResponse;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
@@ -21,16 +19,12 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
                                                                   HttpHeaders headers,
                                                                   HttpStatus status, WebRequest request) {
 
-        Map<String, Object> body = new LinkedHashMap<>();
-
-        body.put("status", "error");
-        body.put("errors", ex.getBindingResult()
+        DealResponse response = new DealResponse("error", ex.getBindingResult()
                 .getFieldErrors()
                 .stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.toList()));
 
-        return new ResponseEntity<>(body, headers, status);
-
+        return new ResponseEntity<>(response, headers, status);
     }
 }
